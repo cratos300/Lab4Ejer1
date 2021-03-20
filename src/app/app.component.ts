909c04cfb03235c6ad9cf9d2c4185a298c15034c
hernan <hernan-vilar@hotmail.com>
2021-03-20 14:20:13 -0300

    Ejercicio1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejercicio1';
+  edadUno:string;
+  edadDos:string;
+  resultado:number = 0;
+  promedioo:number = 0;
 
   HacerCambios()
   {
     this.title = "Cambiado";
+  }
+  suma()
+  {
+    this.resultado = parseInt(this.edadUno) + parseInt(this.edadDos);
+  }
+  promedio()
+  {
+    this.promedioo = (this.resultado / 2);
+  }
+  Realizar()
+  {
+    this.suma();
+    this.promedio();
   }
 }
